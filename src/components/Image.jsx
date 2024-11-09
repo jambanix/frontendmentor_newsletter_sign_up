4134c1f7f3224e7c8819d1d6f1a15a05be489e0b
jambanix <jambanix@pm.me>
2024-11-09 08:30:04 +0000

    tweak image path for vite

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -4,11 +4,10 @@ export const Image = ({ className }) => {
 
   const screenSize = useScreenWidth();
 
-  const imagePath = `/images/illustration-sign-up-${screenSize}.svg`
+  const imagePath = `./images/illustration-sign-up-${screenSize}.svg`
 
   return (
     <section className={`h-full ${className}`}>
-      {/* <div className={`bg-banner-${screenSize} h-full w-full`}></div> */}
       <img src={imagePath} alt="" className="w-full h-full object-cover"/>
     </section>
   )
